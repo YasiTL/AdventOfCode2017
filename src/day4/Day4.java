cf6b5a8d68632930a41a62364062b1d61d867cc0
YasiTL <yasaman.most@gmail.com>
2017-12-13 10:06:38 -0600

    completed Day 4 part 2

--- src/day4/Day4.java
+++ src/day4/Day4.java
@@ -16,24 +16,22 @@ public class Day4 {
 	public static void main(String[] args) throws IOException{
 		List<String> input = Files.readAllLines(Paths.get("day4.in"));
 
-		//		//PART ONE\\
-		//		int total = 0;
-		//		for(int x = 0; x < input.size(); x++){
-		//			String line = input.get(x);
-		//			ArrayList<String> words = new ArrayList<>(Arrays.asList(line.split(" ")));
-		//			for(int word = 0; word < words.size(); word++){
-		//				if (Collections.frequency(words, words.get(word)) > 1){
-		//					total++;
-		//					break;
-		//				}
-		//			}
-		//		}
-		//		System.out.println("Part 1: " + (input.size() - total));
+				//PART ONE\\
+				int total = 0;
+				for(int x = 0; x < input.size(); x++){
+					String line = input.get(x);
+					ArrayList<String> words = new ArrayList<>(Arrays.asList(line.split(" ")));
+					for(int word = 0; word < words.size(); word++){
+						if (Collections.frequency(words, words.get(word)) > 1){
+							total++;
+							break;
+						}
+					}
+				}
+				System.out.println("Part 1: " + (input.size() - total));
 
-		//PART TWO\\
+		//PART TWO\\ Get ready for a really mangled solution :)
 		int total2 = 0;
-		int public_word2= 0;
-		int public_word = 0;
 
 		for(int x = 0; x < input.size(); x++){
 			String line = input.get(x);
@@ -41,26 +39,27 @@ public class Day4 {
 
 			char[] word_array = new char[words.size()];
 			char[] next_word_array = new char[words.size()];
-
+			
+			OUTER_LOOP: //In charge of setting word_array equal to a sorted char array for each word
 			for(int word = 0; word < words.size(); word++){
 				word_array = words.get(word).toCharArray();
 				Arrays.sort(word_array);
-
+				
+				//Sets next_word_array to a sorted char array so we can compare word_array to it
 				for(int word2 = 0; word2 < words.size(); word2++){
+					if(word2 == word){ //Make sure that we arent comparing the same thing
+						continue;
+					}
 					next_word_array = words.get(word2).toCharArray();
 					Arrays.sort(next_word_array);
-
-					System.out.println(Arrays.toString(word_array) + "compared to" + Arrays.toString(next_word_array));
+					//System.out.println(Arrays.toString(word_array) + "compared to" + Arrays.toString(next_word_array));
 					if(Arrays.equals(word_array, next_word_array)){
-						total2 += 1;
-						System.out.println("anagram present");
-						break;
+						total2 ++;
+						break OUTER_LOOP; //Break out of the comparison loops and just go to the next line
 					}
 				}
 			}
 		}
-		System.out.println(total2);
-		System.out.println(input.size());
 		System.out.println("Part 2: " + (input.size() - total2));
 	}
 }
