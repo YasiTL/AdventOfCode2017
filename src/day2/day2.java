75fcb053d1b116dda3083cd2eb834959ea70faec
YasiTL <yasaman.most@gmail.com>
2017-12-15 11:36:25 -0600

    Day 2 part 1 complete

--- src/day2/day2.java
+++ src/day2/day2.java
@@ -14,28 +14,48 @@ import java.util.Scanner;
 public class day2 {
 	public static void main(String[] args) throws IOException{
 		List<String> input = Files.readAllLines(Paths.get("day2.in"));
-
-		int firstNum = 0;
-		int secondNum = 0;
-
+		
 		//PART ONE\\
 		int total = 0;
 		for(int x = 0; x < input.size(); x++){
 			String line = input.get(x);
 			ArrayList<String> numbers = new ArrayList<>(Arrays.asList(line.split("\t")));
-			ArrayList<String> numbers2 = new ArrayList<>(Arrays.asList(line.split("\t")));
+			
+			int max = Integer.parseInt(numbers.get(0));
+			int min = Integer.parseInt(numbers.get(0));
 	
-			for(int number = 0; number < numbers.size(); number++){
-				firstNum = Integer.parseInt(numbers.get(number));
-				for(int j = 0; j < numbers2.size(); j++){
-					secondNum = Integer.parseInt(numbers.get(j));
-					if (firstNum % secondNum == 0 && number != j ){
-						total += firstNum / secondNum;
-						
+			for(int i = 0; i < numbers.size(); i++){
+				if(Integer.parseInt(numbers.get(i)) > max){
+					max = Integer.parseInt(numbers.get(i));
+				}
+				else if(Integer.parseInt(numbers.get(i)) < min){
+					min = Integer.parseInt(numbers.get(i));
 					}
 				}
+			total += max - min;
 			}
-		}
 		System.out.println("Part 1: " + total);
+
+//		//PART TWO\\
+//		int firstNum = 0;
+//		int secondNum = 0;
+//		int total2 = 0;
+//		for(int x = 0; x < input.size(); x++){
+//			String line = input.get(x);
+//			ArrayList<String> numbers = new ArrayList<>(Arrays.asList(line.split("\t")));
+//			ArrayList<String> numbers2 = new ArrayList<>(Arrays.asList(line.split("\t")));
+//	
+//			for(int number = 0; number < numbers.size(); number++){
+//				firstNum = Integer.parseInt(numbers.get(number));
+//				for(int j = 0; j < numbers2.size(); j++){
+//					secondNum = Integer.parseInt(numbers.get(j));
+//					if (firstNum % secondNum == 0 && number != j ){
+//						total2 += firstNum / secondNum;
+//					}
+//				}
+//			}
+//		}
+//		System.out.println("Part 2: " + total2);
+		
 	}
 }
